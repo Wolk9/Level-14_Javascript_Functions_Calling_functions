2a0bdcf33f1be150b50b0bfa6b62b119a20e07fb
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-04-25 17:10:19 +0200

    VAT Exercise 2

--- index.js
+++ index.js
@@ -2,48 +2,83 @@
 
 // Hey Kiddo
 
-const DoAgeCheck = (age) => {
-  if (age >= 18) {
-    return true;
-  }
-  return false;
-};
+// const DoAgeCheck = (age) => {
+//   if (age >= 18) {
+//     return true;
+//   }
+//   return false;
+// };
 
-const GreetProperly = (age) => {
-  const IsAdult = DoAgeCheck(age);
-  if (IsAdult == true) {
-    return "Hello Adult";
-  }
-  return "Hey kiddo";
-};
+// const GreetProperly = (age) => {
+//   const IsAdult = DoAgeCheck(age);
+//   if (IsAdult == true) {
+//     return "Hello Adult";
+//   }
+//   return "Hey kiddo";
+// };
 
-let HowOld = 17;
-console.log(GreetProperly(HowOld));
+// let HowOld = 17;
+// console.log(GreetProperly(HowOld));
 
 // VAT Calculations
 // VAT exercise 1
+
+// const CalculateVAT = (BasePrice, HighLowZeroVAT) => {
+//   //   console.log(BasePrice);
+//   //   console.log(HighLowZeroVAT);
+//   if (HighLowZeroVAT == "High") {
+//     console.log("Hoge BTW");
+//     const VAT = BasePrice * 0.21;
+//     return VAT;
+//   } else if (HighLowZeroVAT == "Low") {
+//     console.log("Lage BTW");
+//     const VAT = BasePrice * 0.09;
+//     return VAT;
+//   } else {
+//     console.log("geen BTW");
+//     const VAT = 0;
+//     return VAT;
+//   }
+// };
+
+// const CalculateTotal = (BasePrice, HighLowZeroVAT) => {
+//   const Total = BasePrice + CalculateVAT(BasePrice, HighLowZeroVAT);
+//   return Total;
+// };
+
+// console.log(CalculateTotal(2000, "Low"));
+// console.log(CalculateTotal(2000, "High"));
+// console.log(CalculateTotal(2000, "Zero"));
+
+// VAT Exercise 2
 
 const CalculateVAT = (BasePrice, HighLowZeroVAT) => {
   //   console.log(BasePrice);
   //   console.log(HighLowZeroVAT);
   if (HighLowZeroVAT == "High") {
-    console.log("Hoge BTW");
-    const VAT = BasePrice * 0.21;
-    return VAT;
+    // console.log("Hoge BTW");
+    const VATHigh = BasePrice * 0.21;
+    return VATHigh;
   } else if (HighLowZeroVAT == "Low") {
-    console.log("Lage BTW");
-    const VAT = BasePrice * 0.09;
-    return VAT;
+    // console.log("Lage BTW");
+    const VATLow = BasePrice * 0.09;
+    return VATLow;
   } else {
-    console.log("geen BTW");
-    const VAT = 0;
-    return VAT;
+    // console.log("geen BTW");
+    const VATZero = 0;
+    return VATZero;
   }
 };
 
 const CalculateTotal = (BasePrice, HighLowZeroVAT) => {
   const Total = BasePrice + CalculateVAT(BasePrice, HighLowZeroVAT);
-  return Total;
+  const VAT = CalculateVAT(BasePrice, HighLowZeroVAT);
+  console.log("---- VAT Calculation -----");
+  console.log("€" + BasePrice);
+  console.log("€" + VAT + "             " + HighLowZeroVAT + "VAT");
+  console.log("_________ +");
+  console.log("€" + Total);
+  return "--------------------------";
 };
 
 console.log(CalculateTotal(2000, "Low"));
